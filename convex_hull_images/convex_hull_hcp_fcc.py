2b1f022de3eac55b2ac3d00c1bf90faede78c354
jonnyli <jli5373@gmail.com>
2022-01-27 16:32:05 -0800

    Updated margins and font sizes on convex hull plot + some miscellaneous jupyter notebook updates

--- convex_hull_images/convex_hull_hcp_fcc.py
+++ convex_hull_images/convex_hull_hcp_fcc.py
@@ -1,3 +1,4 @@
+from turtle import left
 import djlib.clex as cl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,7 +19,12 @@ trash, lower_hull_indices = cl.lower_hull(hull)
 hull_vertices = points[lower_hull_indices]
 hull_vertices = dj.column_sort(hull_vertices, 0)
 
+#TODO: put this under points
+#Check the HCP GS @ 50%
 plt.plot(hull_vertices[:, 0], hull_vertices[:, 1], color="k", label="Convex Hull")
+
+#TODO: Check zorder of the hull
+
 
 # Need to divide formation energy by number of sites per prim
 plt.scatter(
@@ -27,15 +33,19 @@ plt.scatter(
 plt.scatter(
     np.array(fcc_data["comp"]), np.array(fcc_data["formation_energy"]), label="FCC"
 )
+plt.hlines(0, 0, 1, linestyles="dashed",color="k")
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
 
+#TODO: Increase font size of scaling
 
 plt.legend(fontsize=15)
-plt.title(r"ZrN$_{x}$ HCP and FCC Convex Hull", fontsize=30)
+#plt.title(r"ZrN$_{x}$ HCP and FCC Convex Hull", fontsize=30)
 plt.xlabel("Composition (x)", fontsize=20)
 plt.ylabel("Formation Energy per Zr (eV)", fontsize=20)
 
 fig = plt.gcf()
 fig.set_size_inches(8, 6)
+plt.subplots_adjust(left=0.15,bottom=0.11)
 
 plt.show()
-
